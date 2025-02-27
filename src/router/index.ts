80b6db712c4b6aa79c0665a0986f0044a893ea84
Ryan Mooney <ryan@texrus.com>
2025-02-27 13:34:44 -0900

    add dynamic routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,6 +15,11 @@ const router = createRouter({
       name: 'projects',
       component: () => import('@/views/ProjectsView.vue'),
     },
+    {
+      path: '/projects/:id',
+      name: 'projectView',
+      component: () => import('@/views/SingleProjectView.vue'),
+    },
   ],
 })
 
