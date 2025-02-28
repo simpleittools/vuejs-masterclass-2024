62ec7fbcc08247a8b800ed900f500874cdfece46
Ryan Mooney <ryan@texrus.com>
2025-02-28 09:13:44 -0900

    Add .env file to hold Supabase info. Create .env.example to be a template file

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
-  'https://yvtkfwkbodijdoategng.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2dGtmd2tib2RpamRvYXRlZ25nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA3NjMyMDYsImV4cCI6MjA1NjMzOTIwNn0.XVZIa_eyJKujtot07o3XQFEdJ7-FaDnFp7Ulm4dGx30',
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY,
 )
