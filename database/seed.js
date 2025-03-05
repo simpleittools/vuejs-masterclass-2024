c2791c1305574c015375dc4f0068076351c9a6be
Ryan <wm.ryan.mooney@gmail.com>
2025-03-04 16:52:31 -0900

    add array to seed multiple items

--- database/seed.js
+++ database/seed.js
@@ -8,14 +8,20 @@ const supabase = createClient(
   process.env.SERVICE_ROlE_KEY,
 )
 
-const seedProjects = async() => {
-  const name = faker.lorem.words(3)
-  await supabase.from('projects').insert({
-    name: name,
-    slug: name.toLowerCase().replace(/ /g, '-'),
-    status: faker.helpers.arrayElement(['in-progress', 'completed']),
-    collaborators: faker.helpers.arrayElements([1,2,3])
-  })
+const seedProjects = async(numEntries) => {
+  const projects = []
+
+  for (let i = 0; i < numEntries; i++){
+    const name = faker.lorem.words(3)
+    projects.push({
+      name: name,
+      slug: name.toLowerCase().replace(/ /g, '-'),
+      status: faker.helpers.arrayElement(['in-progress', 'completed']),
+      collaborators: faker.helpers.arrayElements([1,2,3])
+    })
+  }
+  await supabase.from('projects').insert(projects)
+
 }
 
-await seedProjects()
+await seedProjects(10)
